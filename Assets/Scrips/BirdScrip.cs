b9ca79ce964ef10741584dcbb6c5e070c529f972
ReaperDream <henryt.o@hotmail.com>
2018-01-17 09:02:46 -0600

    Flappy Bird

---
+++ Assets/Scrips/BirdScrip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScrip : MonoBehaviour {
+
+    float forceBird = 10f;
+    float forwardSpeed = 2f;
+    Rigidbody2D birb;
+    GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+        birb = GetComponent<Rigidbody2D>();
+        cam = GameObject.Find("Main Camera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        birb.velocity = Vector2.zero;
+        if (Input.GetKey(KeyCode.Space)) {
+            birb.AddForce(Vector2.up * forceBird);
+            birb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+        }
+        cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+
+    }
+}
